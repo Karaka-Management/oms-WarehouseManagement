07f3e6439b2fcebe5dd7f165db34d4a0835a4eb2
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-09-19 21:34:04 +0200

    Split controllers per application

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -12,7 +12,7 @@
  */
 declare(strict_types=1);
 
-namespace Modules\WarehouseManagement;
+namespace Modules\WarehouseManagement\Controller;
 
 use Modules\Navigation\Models\Navigation;
 use Modules\Navigation\Views\NavigationView;
@@ -30,7 +30,7 @@ use phpOMS\Module\WebInterface;
  * @link       http://website.orange-management.de
  * @since      1.0.0
  */
-final class Controller extends ModuleAbstract implements WebInterface
+class Controller extends ModuleAbstract implements WebInterface
 {
 
     /**
@@ -39,7 +39,7 @@ final class Controller extends ModuleAbstract implements WebInterface
      * @var string
      * @since 1.0.0
      */
-    public const MODULE_PATH = __DIR__;
+    public const MODULE_PATH = __DIR__ . '/../';
 
     /**
      * Module version.
