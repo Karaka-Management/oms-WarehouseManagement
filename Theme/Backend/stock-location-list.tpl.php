cd45dc6f9f16ef48cf460712970bb93b4c002ba2
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-04-18 22:29:46 +0200

    new form layout and other tpl fixes

--- Theme/Backend/stock-location-list.tpl.php
+++ Theme/Backend/stock-location-list.tpl.php
@@ -22,7 +22,7 @@ echo $this->getData('nav')->render(); ?>
     <div class="col-xs-12">
         <div class="portlet">
             <div class="portlet-head"><?= $this->getHtml('Locations'); ?><i class="fa fa-download floatRight download btn"></i></div>
-            <table id="stockList" class="default">
+            <table id="stockList" class="default sticky">
                 <thead>
                 <tr>
                     <td><?= $this->getHtml('ID', '0', '0'); ?>
