4a70dd13fd6400d406b05552a5f549b9faaf1779
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-10-03 09:16:47 +0200

    phpcs, static and unit test fixes

--- Admin/Installer.php
+++ Admin/Installer.php
@@ -35,6 +35,14 @@ use Modules\WarehouseManagement\Models\StockLocationMapper;
 final class Installer extends InstallerAbstract
 {
     /**
+     * Path of the file
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    public const PATH = __DIR__;
+
+    /**
      * {@inheritdoc}
      */
     public static function install(DatabasePool $dbPool, ModuleInfo $info, SettingsInterface $cfgHandler) : void
