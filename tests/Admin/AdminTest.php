35a625bd7416390120a6c068d8ae73ac2244265c
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-10-21 22:19:51 +0200

    make classes final

--- tests/Admin/AdminTest.php
+++ tests/Admin/AdminTest.php
@@ -17,7 +17,7 @@ namespace Modules\WarehouseManagement\tests\Admin;
 /**
  * @internal
  */
-class AdminTest extends \PHPUnit\Framework\TestCase
+final class AdminTest extends \PHPUnit\Framework\TestCase
 {
     protected const NAME = 'WarehouseManagement';
 
