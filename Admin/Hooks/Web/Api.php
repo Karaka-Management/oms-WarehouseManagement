db9736b65265cdb3adef61c1a60ef4817f1b972d
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-02-10 18:20:57 +0100

    many small fixes

--- Admin/Hooks/Web/Api.php
+++ Admin/Hooks/Web/Api.php
@@ -13,13 +13,4 @@
 declare(strict_types=1);
 
 return [
-    'POST:Module:ClientManagement-client-create' => [
-        'callback' => ['\Modules\WarehouseManagement\Controller\ApiController:eventStockCreateInternal'],
-    ],
-    'POST:Module:SupplierManagement-supplier-create' => [
-        'callback' => ['\Modules\WarehouseManagement\Controller\ApiController:eventStockCreateInternal'],
-    ],
-    '/POST:Module:Billing\-bill(_element)*\-(create|delete|update)/' => [
-        'callback' => ['\Modules\WarehouseManagement\Controller\ApiController:eventBillUpdateInternal'],
-    ],
 ];
