0dbce803adf71849143ab12fc3d0cd1b68a2bfe5
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-25 01:12:31 +0000

    test fixes (NO_CI)

--- tests/Admin/AdminTest.php
+++ tests/Admin/AdminTest.php
@@ -23,5 +23,5 @@ final class AdminTest extends \PHPUnit\Framework\TestCase
 
     protected const URI_LOAD = 'http://127.0.0.1/en/backend/warehouse';
 
-    use \Build\Helper\ModuleTestTrait;
+    use \tests\Modules\ModuleTestTrait;
 }
